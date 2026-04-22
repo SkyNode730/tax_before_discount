b445b972675d73d264bee8b37db4fff3e19d5bb1
SafdarAliGit <safdar211@gmail.com>
2026-04-22 17:13:48 +0500

    udpated

--- tax_before_discount/public/js/sales_invoice.js
+++ tax_before_discount/public/js/sales_invoice.js
@@ -13,5 +13,11 @@ frappe.ui.form.on("Sales Invoice", {
                 }
             }
         );
+    },
+    is_return:function(frm){
+        if(frm.doc.is_return){
+            frm.set_value("update_stock", 1);
+        }
     }
+    
 });
